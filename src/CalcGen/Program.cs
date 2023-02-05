e18d657d0136410ba0b256edccea9b8eadc3d639
Labasse <labasse@gmail.com>
2023-02-05 09:13:48 +0000

    Généraliser les calculs (début)

---
+++ src/CalcGen/Program.cs
@@ -0,0 +1,11 @@
+﻿void FibonacciAB(int nombre, int a, int b)
+{
+  for (; nombre-- > 0; a = b - a)
+    Console.Write($"{b += a} ");
+}
+
+void Fibonacci(int nombre)
+    => FibonacciAB(nombre, 0, 1);
+
+Fibonacci(10);
+Console.WriteLine();
