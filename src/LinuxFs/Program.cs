d8cecdb0f6d4294b889cf866061f6904fae6ff7c
Labasse <labasse@gmail.com>
2023-02-05 08:51:51 +0000

    Traiter les archives Linux (début)

---
+++ src/LinuxFs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Formats.Tar;
+using System.IO.Compression;
+
+var racine = Path.Combine(
+    Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) ?? "./",
+    "../../.."
+);
+var archive = Path.Combine(racine, "archive.tar.gz");
+var repertoire = Path.Combine(racine, "obj");
+var contenu = Path.Combine(racine, "obj/contenu");
+
+try
+{
+  Console.WriteLine("Décompression : ");
+
+  using (var gz = new GZipStream(File.OpenRead(archive), CompressionMode.Decompress))
+  {
+    // TODO : Extraire le contenu du fichier Tar
+  }
+
+  Console.WriteLine("Parcours :");
+  foreach (var chemin in Directory.EnumerateFileSystemEntries(contenu))
+  {
+    var info = "...";
+    Console.WriteLine($"- {Path.GetFileName(chemin)} : {info}");
+  }
+}
+catch (IOException e)
+{
+  Console.Error.WriteLine(e.Message);
+}
